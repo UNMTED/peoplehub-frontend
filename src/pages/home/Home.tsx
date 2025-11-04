6da493183afe3c7e02645cb301476a4542589130
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-04 15:40:05 -0300

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <>
+            <main className="h-screen text-white text-center bg-linear-to-bl from-slate-800 to-slate-500">
+                Home
+            </main>
+        </>
+    );
+};
