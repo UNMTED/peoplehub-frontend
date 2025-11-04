9d596672f8ffa7401eecbba7b3592057877a026b
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-04 17:39:47 -0300

    style: ajuste do espaçamente entre as palavras

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,13 +5,13 @@ export const Home = () => {
         <>
             <div className="bg-[url(src/assets/images/imagem_de_fundo.png)] bg-top-right bg-no-repeat h-full">
                 <NavBar />
-                <main className="  mt-20 font-[Poppins] tracking-widest">
+                <main className="  mt-20 font-[Poppins] [word-spacing:0.2rem] tracking-widest">
                     <section className="w-1/2 flex flex-col gap-10">
                         <h1 className="text-[48px]">
                             Sua Solução em <br /> Gestão <br /> de Capital
                             Humano
                         </h1>
-                        <p className="text-2xl">
+                        <p className="text-2xl w-4/5">
                             Simplifique os processos de RH, conecte pessoas e
                             impulsione o sucesso da sua organização com
                             tecnologia e empatia.
