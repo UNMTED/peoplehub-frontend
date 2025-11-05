7f07690566a7f52a9d3951043656d7b67ade6063
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-05 11:42:26 -0300

    refactor: troca rota de produto por solucao

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import { NavBar } from "./components/navbar/NavBar";
 import Contato from "./pages/contato/Contato";
 import { Home } from "./pages/home/Home";
 import NossoTime from "./pages/nossotime/NossoTIme";
 import { Sobre } from "./pages/sobre/Sobre";
-<<<<<<< HEAD
 import Solucao from "./pages/solucao/Solucao";
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <div className="min-h-screen px-20 font-[Poppins] text-white bg-linear-to-bl from-people-color-dark to-people-color-light">
-                    <div className="max-w-7xl mx-auto">
+                <div className="min-h-screen  font-[Poppins] text-white bg-linear-to-bl from-people-color-dark to-people-color-light">
+                    <div className=" px-20 mx-auto">
                         <NavBar />
                         <Routes>
                             <Route
@@ -37,34 +37,11 @@ function App() {
                             />
                         </Routes>
                     </div>
+                    <Footer />
                 </div>
             </BrowserRouter>
         </>
     );
-=======
-import Footer from "./components/footer/Footer";
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <div className="min-h-screen  font-[Poppins] text-white bg-linear-to-bl from-people-color-dark to-people-color-light">
-          <div className=" px-20 mx-auto">
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/sobre" element={<Sobre />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/nosso-time" element={<NossoTime />} />
-              <Route path="/produto" element={<Produto />} />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </>
-  );
->>>>>>> 4b9c5e94f675b794ed90792aff19b3f604d8c7be
 }
 
 export default App;
