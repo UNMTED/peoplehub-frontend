1012f537b9f7551d9f3e6d132117978edfebc04a
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-05 14:15:40 -0300

    style: ajuste de tamanho de fontes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,11 +4,11 @@ export const Home = () => {
             <div className="bg-[url(src/assets/images/imagem_de_fundo.png)] bg-contain bg-top-right bg-no-repeat  min-h-[85vh]">
                 <main className="mt-20 font-[Poppins] [word-spacing:0.2rem] tracking-widest">
                     <section className="w-1/2 flex flex-col gap-10">
-                        <h1 className="text-[48px]">
+                        <h1 className="text-4xl">
                             Sua Solução em <br /> Gestão <br /> de Capital
                             Humano
                         </h1>
-                        <p className="text-2xl w-4/5">
+                        <p className="text-lg w-4/5">
                             Simplifique os processos de RH, conecte pessoas e
                             impulsione o sucesso da sua organização com
                             tecnologia e empatia.
